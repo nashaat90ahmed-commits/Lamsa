874ad387cd7b788bf0386085c583b9cab5487e2a
nashaat90ahmed-commits <nashaat90ahmed@gmail.com>
2026-01-02 00:22:54 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,26 +1,16 @@
-const CACHE_NAME = 'lamsa-pwa-v1';
-const urlsToCache = [
-  '/',
-  '/index.html',
-  '/manifest.json',
-  '/icons/icon-192.png',
-  '/icons/icon-512.png'
-];
+const CACHE_NAME = 'lamsa-v1';
+const urlsToCache = ['/', '/style.css', '/script.js'];
 
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then(cache => {
-        return cache.addAll(urlsToCache);
-      })
+      .then(cache => cache.addAll(urlsToCache))
   );
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
-      .then(response => {
-        return response || fetch(event.request);
-      })
+      .then(response => response || fetch(event.request))
   );
 });
