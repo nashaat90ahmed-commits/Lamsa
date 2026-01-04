80960ce68baa1b9c40cedd78d50a22964bfebf53
nashaat90ahmed-commits <nashaat90ahmed@gmail.com>
2026-01-04 02:23:32 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -7,15 +7,23 @@ export default defineConfig({
     react(),
     VitePWA({
       registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true // لاختبار PWA في التطوير
+      },
       manifest: {
         name: 'لمسة',
         short_name: 'لمسة',
-        description: 'لقاء يبدأ بلمسة...',
+        description: 'تطبيق عربي فاخر للتعارف والزواج.',
         theme_color: '#D4AF37',
+        background_color: '#0F1C3F',
+        display: 'standalone',
+        scope: '/',
+        start_url: '/',
         icons: [
           { src: '/icons/icon-192.png', sizes: '192x192', type: 'image/png' },
           { src: '/icons/icon-512.png', sizes: '512x512', type: 'image/png' }
-        ]
+        ],
+        lang: 'ar'
       }
     })
   ],
